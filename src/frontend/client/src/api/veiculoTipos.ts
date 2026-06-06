7d622c6ca1c069c2dc9ebeb03a66c68edf47f32c
Valdeir Mendes <valdeircarlosm@hotmail.com>
2026-06-06 17:04:49 -0300

    Update tipo de veiculos frontend

--- src/frontend/client/src/api/veiculoTipos.ts
+++ src/frontend/client/src/api/veiculoTipos.ts
@@ -1,4 +1,4 @@
 import { createCrudApi } from "./crud";
 import type { TipoVeiculoApi, TipoVeiculoPayload } from "./types";
 
-export const  tiposVeiculoApi = createCrudApi<TipoVeiculoApi, TipoVeiculoApi>("/tipos/veiculos");
+export const  tiposVeiculoApi = createCrudApi<TipoVeiculoApi, TipoVeiculoPayload>("/tipos/veiculos");
